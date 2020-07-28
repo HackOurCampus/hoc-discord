6f138021914f8b93b4efd30a90c42aaec1fbca6d
Ansh Godha <ag759@cornell.edu>
2020-07-28 15:56:16 +0530

    Add channel linking

--- index.js
+++ index.js
@@ -3,11 +3,33 @@ const Discord = require("discord.js");
 const welcome = require("./welcome");
 const client = new Discord.Client();
 
+const channelNames = ["general", "brainstorming", "logistics", "workshops"];
+
 client.on("guildMemberAdd", (member) => {
   const username = member.user.username;
-  const message = welcome(username);
+  let channelIDMap = {};
+  channelNames.forEach((name) => {
+    const id = member.guild.channels.cache
+      .find((channel) => channel.name === name)
+      .toString();
+    channelIDMap[name] = id;
+  });
+
+  const message = welcome(username, channelIDMap);
   member.send(message);
-  //
+});
+
+// debugging only
+client.on("message", (message) => {
+  let channelIDMap = {};
+  channelNames.forEach((name) => {
+    const id = message.guild.channels.cache
+      .find((channel) => channel.name === name)
+      .toString();
+    channelIDMap[name] = id;
+  });
+  if (message.content === "HOCInfo")
+    message.reply(welcome(message.author.username, channelIDMap));
 });
 
 client.login(process.env.BOT_TOKEN);
--- welcome.js
+++ welcome.js
@@ -1,12 +1,12 @@
-module.exports = (name) => {
+module.exports = (name, channelIDOBj) => {
   return `Welcome to HackOurCampus, ${name}! We're glad to have you here. Here's a quick overview of what is going on in this Discord server:\n
-- <#732829032575664251>: This is our central hub. We will be using this channel to make announcements and answer general questions about HackOurCampus and to socialize and form a 🔥 community!
+- ${channelIDOBj["general"]}: This is our central hub. We will be using this channel to make announcements and answer general questions about HackOurCampus and to socialize and form a 🔥 community!
     
-- <#732833434183008330>: Here, we will be coordinating submission dates/times, giving information about speakers/workshops, and answering any questions you might have about how this hackathon is structured! 🕑
+- ${channelIDOBj["logistics"]}: Here, we will be coordinating submission dates/times, giving information about speakers/workshops, and answering any questions you might have about how this hackathon is structured! 🕑
 
-- <#732833475257696276>: In the coming weeks, we will coordinate virtual hangouts for all hackers so they can come together and think of dope ideas 💡
+- ${channelIDOBj["brainstorming"]}: In the coming weeks, we will coordinate virtual hangouts for all hackers so they can come together and think of dope ideas 💡
 
-- <#732833623971069952>: We will be periodically updating that channel whenever a workshop is about to start. You can also interact with our speakers for a short while after their workshop has ended 🤓
+- ${channelIDOBj["workshops"]}: We will be periodically updating that channel whenever a workshop is about to start. You can also interact with our speakers for a short while after their workshop has ended 🤓
 
 You can also expect more channels more tailored to your hackathon needs, such as channels with your mentor, and maybe even your hackathon group.
     
